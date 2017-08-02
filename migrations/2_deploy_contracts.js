c208bd25fef0a86e3f0bf0734032b7cc62cfeae6
Andrew Trask <liamtrask@gmail.com>
2017-08-02 04:27:37 +0100

    updated build scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ModelRepository = artifacts.require("./ModelRepository.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
+  deployer.deploy(ModelRepository);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+
 };
