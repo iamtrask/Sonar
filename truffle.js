1b4c376229039da882a13bf4a1a212895be60c61
Alex Johnson <hello@alex-johnson.net>
2017-08-09 14:51:45 -0700

    create a separate docker network

--- truffle.js
+++ truffle.js
@@ -1,9 +1,14 @@
 module.exports = {
   networks: {
     development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "default"
+    },
+    docker: {
       host: "testrpc",
       port: 8545,
-      network_id: "*"
+      network_id: 25189
     }
   }
 };
