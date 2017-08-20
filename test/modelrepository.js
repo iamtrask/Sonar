63838d77f044f5fa179047bd587e0c8b3edac0fd
Axel Hodler <axel@hodler.co>
2017-08-20 14:01:14 +0200

    verify persisting model works

--- test/modelrepository.js
+++ test/modelrepository.js
@@ -1,9 +1,38 @@
 var ModelRepository = artifacts.require('./ModelRepository.sol')
 
-contract('ModelRepository', function(accounts) {
-  it('can be deployed', function() {
-    return ModelRepository.deployed().then(function(instance) {
-      assert.ok(instance !== undefined)
+const hexToString = hexString => {
+  return new Buffer(hexString.replace('0x', ''), 'hex').toString()
+}
+
+contract('ModelRepository', accounts => {
+  const ulyssesTheUser = accounts[0]
+
+  it('allows anyone to add a model', () => {
+    const ipfsHash = 'QmWmyoMoctfbAaiEs2G46gpeUmhqFRDW6KWo64y5r581Vz'
+    const twoPartIpfsHash = [
+      ipfsHash.substring(0, 32),
+      ipfsHash.substr(32) + '0'.repeat(18)
+    ]
+    const bountyInWei = 10000
+    const initialError = 42
+    const targetError = 1337
+
+    let modelRepositoryContract;
+    return ModelRepository.deployed().then(instance => {
+      modelRepositoryContract = instance;
+      return modelRepositoryContract.addModel(twoPartIpfsHash, initialError, targetError, {
+        from: ulyssesTheUser,
+        value: bountyInWei
+      })
+    }).then(() => {
+      return modelRepositoryContract.getModel.call(0)
+    }).then(model => {
+      assert.equal(model[0], ulyssesTheUser, 'owner persisted')
+      assert.equal(model[1], bountyInWei, 'bounty persisted')
+      assert.equal(model[2], initialError, 'initial_error persisted')
+      assert.equal(model[3], targetError, 'target_error persisted')
+      assert.equal(hexToString(model[4][0]), twoPartIpfsHash[0], 'ipfshash persisted')
+      assert.equal(hexToString(model[4][1]), twoPartIpfsHash[1], 'ipfshash persisted')
     })
   })
 })
