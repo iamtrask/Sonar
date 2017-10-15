ad3f1ac43bbe26691bf05c73978d0ef44e9d3999
bendangnuksung <bendangnuksungimsong@gmail.com>
2017-10-15 16:26:10 +0530

    correction on testmodelrepo

--- test/TestModelRepository.sol
+++ test/TestModelRepository.sol
@@ -17,19 +17,19 @@ contract TestModelRepository {
   function testIncentiveValue() {
     ModelRepository repo = new ModelRepository();
 
-    bounty_1 = 10000;
-    bounty_2 = 123456789;
+    uint bounty_1 = 10000;
+    uint bounty_2 = 123456789;
 
     //total error = initial error - target_error
-    totalError_1 = 2000;
-    totalError_2 = 9;
+    uint totalError_1 = 2000;
+    uint totalError_2 = 9;
 
     //solved error = best_error - _new_model_error
-    solvedError_1 = 37;
-    solvedError_2 = 4;
+    uint solvedError_1 = 37;
+    uint solvedError_2 = 4;
 
-    solutionValue_1 = 185;
-    solutionValue_2 = 54869684;
+    uint solutionValue_1 = 185;
+    uint solutionValue_2 = 54869684;
 
     Assert.equal(repo.calculateIncentive(bounty_1, totalError_1, solvedError_1), solutionValue_1, "calculateIncentive 1");
     Assert.equal(repo.calculateIncentive(bounty_2, totalError_2, solvedError_2), solutionValue_2, "calculateIncentive 2");
