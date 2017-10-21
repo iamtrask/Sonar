fec28fcc34405d7c208a02b94d6cd9bc76d52f57
anoff <offenhaeuser@gmail.com>
2017-10-21 14:46:32 +0200

    :ant: fix: fix lint errors

--- truffle.js
+++ truffle.js
@@ -11,9 +11,9 @@ module.exports = {
       network_id: 25189
     },
     rinkeby: {
-      host: "localhost", // Connect to geth on the specified
+      host: 'localhost', // Connect to geth on the specified
       port: 8545,
-      from: "0xbf4696ecfa2d3697f98805d4166fdaeaf3b67944", // default address to use for any transaction Truffle makes during migrations
+      from: '0xbf4696ecfa2d3697f98805d4166fdaeaf3b67944', // default address to use for any transaction Truffle makes during migrations
       network_id: 4,
       gas: 4612388 // Gas limit used for deploys
     }
