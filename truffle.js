9d23f2f9a9eed803dbeff57c48d7c9f6437d2435
anoff <offenhaeuser@gmail.com>
2017-10-21 14:13:15 +0200

    :page_facing_up: docs: add deployment instructions

--- truffle.js
+++ truffle.js
@@ -9,6 +9,13 @@ module.exports = {
       host: 'testrpc',
       port: 8545,
       network_id: 25189
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0xbf4696ecfa2d3697f98805d4166fdaeaf3b67944", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 }
