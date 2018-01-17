99dff699863e82ea0246843aa154f0a5168779a0
Justin Patriquin <justin@sidestory.email>
2018-01-17 15:49:34 -0400

    jobs array to experiment

--- contracts/TrainingGrid.sol
+++ contracts/TrainingGrid.sol
@@ -7,31 +7,33 @@ contract TrainingGrid {
     address owner;
     uint bounty;
     bytes32[] ipfs;
+    bytes32[] jobs;
   }
 
   mapping(bytes32 => Experiment) experiments;
   bytes32[] experimentIds;
 
-  function addExperiment(bytes32[] experimentAddress) public payable {
+  function addExperiment(bytes32[] experimentAddress, bytes32[] jobIds) public payable {
     Experiment memory experiment;
     experiment.id = keccak256(experimentAddress);
     experiment.owner = msg.sender;
     experiment.bounty = msg.value;
     experiment.ipfs = experimentAddress;
+    experiment.jobs = jobIds;
 
     experiments[experiment.id] = experiment;
     experimentIds.push(experiment.id);
   }
 
-  function countExperiments() constant public returns (uint256 experimentCount) {
+  function countExperiments() public constant returns (uint256 experimentCount) {
     return experimentIds.length;
   }
 
-  function getExperimentIds() constant public returns (bytes32[]) {
+  function getExperimentIds() public constant returns (bytes32[]) {
     return experimentIds;
   }
 
-  function getExperiment(bytes32[] experimentAddress) constant public returns (address, uint, bytes32[]) {
+  function getExperiment(bytes32[] experimentAddress) public constant returns (address, uint, bytes32[]) {
     Experiment memory experiment;
     bytes32 id = keccak256(experimentAddress);
     experiment = experiments[id];
